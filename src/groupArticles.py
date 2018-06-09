02c80251d7514ea9c5c3483c58591497c919d597
Sebastian Gerard <s.gerard@gmx.net>
2018-06-09 12:44:21 +0200

    ibm watson added

--- src/groupArticles.py
+++ src/groupArticles.py
@@ -6,3 +6,29 @@ data = pd.read_csv("../data/newsCorpora.csv", delimiter = "\t", names = ["id", "
 
 data = data[data.headline.map(lambda headline: keyword in headline)]
 
+
+import json
+from watson_developer_cloud import NaturalLanguageUnderstandingV1
+from watson_developer_cloud.natural_language_understanding_v1 \
+  import Features, EntitiesOptions, KeywordsOptions
+
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+  username='04212652-f78b-4b0c-bdbf-ea9bc87b5847',
+  password='UPMKxSR8ZroF',
+  version='2018-03-16')
+
+response = natural_language_understanding.analyze(
+  text='IBM is an American multinational technology company '
+       'headquartered in Armonk, New York, United States, '
+       'with operations in over 170 countries.',
+  features=Features(
+    entities=EntitiesOptions(
+      emotion=True,
+      sentiment=True,
+      limit=2),
+    keywords=KeywordsOptions(
+      emotion=True,
+      sentiment=True,
+      limit=2)))
+
+print(json.dumps(response, indent=2))
