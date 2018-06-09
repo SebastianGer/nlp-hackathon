8d5178ed82cb60db0ed6a46df351bb46cb0b3c69
Sebastian Gerard <s.gerard@gmx.net>
2018-06-09 12:25:12 +0200

    filtering headlines for keyword

--- src/groupArticles.py
+++ src/groupArticles.py
@@ -2,7 +2,7 @@ import sys
 import pandas as pd
 
 keyword = "test"
-data = pd.read_csv("../data/newsCorpora.csv", delimiter = "\t", , names = ["id", "headline", "url", "website-name", "dontknow", "dontknow2", "domain", "timestamp"])
+data = pd.read_csv("../data/newsCorpora.csv", delimiter = "\t", names = ["id", "headline", "url", "website-name", "dontknow", "dontknow2", "domain", "timestamp"])
 
-data = data[keyword in data["headline"]]
+data = data[data.headline.map(lambda headline: keyword in headline)]
 
