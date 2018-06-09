1eff1b0732041edc07ee54ec8612519df345d480
Sebastian Gerard <s.gerard@gmx.net>
2018-06-09 13:14:59 +0200

    watson call works now

--- src/groupArticles.py
+++ src/groupArticles.py
@@ -1,11 +1,10 @@
-import sys
 import pandas as pd
 
 keyword = "test"
 data = pd.read_csv("../data/newsCorpora.csv", delimiter = "\t", names = ["id", "headline", "url", "website-name", "dontknow", "dontknow2", "domain", "timestamp"])
 
 data = data[data.headline.map(lambda headline: keyword in headline)]
-
+data.reset_index(drop = True, inplace = True)
 
 import json
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
@@ -15,12 +14,11 @@ from watson_developer_cloud.natural_language_understanding_v1 \
 natural_language_understanding = NaturalLanguageUnderstandingV1(
   username='04212652-f78b-4b0c-bdbf-ea9bc87b5847',
   password='UPMKxSR8ZroF',
-  version='2018-03-16')
+  version='2018-03-16',
+  url='https://gateway-fra.watsonplatform.net/natural-language-understanding/api')
 
 response = natural_language_understanding.analyze(
-  text='IBM is an American multinational technology company '
-       'headquartered in Armonk, New York, United States, '
-       'with operations in over 170 countries.',
+  url=data["url"][0],
   features=Features(
     entities=EntitiesOptions(
       emotion=True,
